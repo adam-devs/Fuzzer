4dc0bed954400970c43584cb385b8d19a5cbfdce
Adam Alilou <aa1320@ic.ac.uk>
2024-01-31 18:25:46 +0000

    more error types

--- process_output.cpp
+++ process_output.cpp
@@ -14,14 +14,18 @@ undefined_behaviour_t process_output(std::string output) {
 	} else if (std::string::npos != output.find(OUTPUT_HEAP_BUFFER_OVERFLOW)){
 		return heap_buffer_overflow;
 	} else if (std::string::npos != output.find(OUTPUT_WILD_POINTER)){
-	    return wild_pointer;
-	} else if (std::string::npos != output.find(SAT)){
-	    return no_error;
+	  return wild_pointer;
   } else if (std::string::npos != output.find(MISALIGNED_ADDRESS)) {
-	    return alignment;
-  } else if (std::string::npos != output.find(UNSAT))
-	    return no_error;  
-  else {
+	  return alignment;
+ 	} else if (std::string::npos != output.find(SEG_FAULT)){
+    return seg_fault;
+  } else if (std::string::npos != output.find(SANITIZER)){
+    return error;
+  } else if (std::string::npos != output.find(SAT)){
+	  return no_error;
+  } else if (std::string::npos != output.find(UNSAT)){
+	  return no_error;  
+  } else {
 		return uncategorized;
 	}	
 }
